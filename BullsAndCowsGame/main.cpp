94622171ee94e65fd20b94126f201b24b4c0746d
Nick Duncan <nickeduncan@gmail.com>
2017-04-01 01:15:34 -0400

    Initial Commit

---
+++ BullsAndCowsGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BullsAndCowsGame
+//
+//  Created by Nicholas Duncan on 4/1/17.
+//  Copyright © 2017 Nicholas Duncan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
