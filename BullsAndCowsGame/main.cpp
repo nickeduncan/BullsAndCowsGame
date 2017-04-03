4e90374d569af6a56b1a73a29f34a02a6a9b6b82
Nick Duncan <nickeduncan@gmail.com>
2017-04-02 23:58:08 -0400

    replaced string with FString and int with int32 to be more in line with unreal standards

--- BullsAndCowsGame/main.cpp
+++ BullsAndCowsGame/main.cpp
@@ -1,10 +1,17 @@
+/* This is the console executable, that makes use of the BullCow class
+This acts as the view in a MVC pattern, and is responsible for all
+user interaction */
+
 #include <iostream>
 #include <string>
 #include "FBullCowGame.hpp"
 
+using FText = std::string;
+using int32 = int;
+
 void PrintIntro();
 void PlayGame();
-std::string GetGuess();
+FText GetGuess();
 bool AskToPlayAgain();
 
 FBullCowGame BCGame; // instantiate a new game
@@ -25,7 +32,7 @@ int main() {
 
 // introduce the game
 void PrintIntro() {
-    constexpr int WORD_LENGTH = 9;
+    constexpr int32 WORD_LENGTH = 9;
     std::cout << "Welcome to Bulls and Cows.\n";
     std::cout << "Can you guess what " << WORD_LENGTH;
     std::cout << " letter isogram I'm thinking of?\n";
@@ -35,13 +42,13 @@ void PrintIntro() {
 void PlayGame() {
 
     BCGame.Reset();
-    int MaxTries = BCGame.GetMaxTries();
+    int32 MaxTries = BCGame.GetMaxTries();
     std::cout << std::endl;
 
     // loop for number of turns asking for guesses
     // TODO: change from FOR to WHILE loop once we are validating tries
-    for (int count = 1; count <= MaxTries; count++) {
-        std::string Guess = GetGuess(); // TODO: make loop checking valid
+    for (int32 count = 1; count <= MaxTries; count++) {
+        FText Guess = GetGuess(); // TODO: make loop checking valid
 
         // submit valid guess to the game
         // print number of bulls and cows
@@ -53,18 +60,18 @@ void PlayGame() {
 }
 
 // get a guess from the player
-std::string GetGuess() {
-    int CurrentTry = BCGame.GetCurrentTry();
+FText GetGuess() {
+    int32 CurrentTry = BCGame.GetCurrentTry();
 
     std::cout << "Try " << CurrentTry << ". Enter your guess: ";
-    std::string Guess = "";
+    FText Guess = "";
     std::getline(std::cin, Guess);
     return Guess;
 }
 
 bool AskToPlayAgain() {
     std::cout << "Do you want to play again (y/n)?";
-    std::string Response = "";
+    FText Response = "";
     std::getline(std::cin, Response);
     return (Response[0] == 'y' || Response[0] == 'Y');
 }
